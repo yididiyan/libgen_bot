7bf18c8d87901b4e269a5d02fc34e012fc907c00
Yididya <yididiya.yilma@gmail.com>
2018-03-02 14:07:41 +0300

    Add chat `typing` actions

--- libgen_bot/handlers.py
+++ libgen_bot/handlers.py
@@ -10,26 +10,20 @@ from telegram import ChatAction
 
 
 def start(bot, update):
-    import ipdb ; ipdb.set_trace()
-    bot.send_message(chat_id=update.messsage.chat_id, text='Im the bot you have been looking for quite sometime!!')
+    bot.send_message(chat_id=update.message.chat_id, text='Im the bot you have been looking for quite sometime!!')
 
-def search(bot, update, args):
-    query_text = ' '.join(args)
-    
-    
-    results = api.search(query_text)
-    # import ipdb ; ipdb.set_trace()
-    bot.send_message(chat_id=update.message.chat_id, text=(emojize_book_list(results) if results  else 'No results Found!!'), parse_mode=ParseMode.HTML)
-
-def fetch(bot, update):
-    pass
+def fetch(bot, update, **kwargs):
+    md5 = update.effective_message.text[4:]
+    url = api.fetch(md5)
+    bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+    bot.send_message(chat_id=update.message.chat_id, text=url)
 
 def emojize_book_list(books):
-    return '\n'.join(map(lambda book: emojize(':blue_book:' + book['title'] + book['author'] + '</br><a href="/get' + book['md5'] + '">[/get'+ book['md5'] +']</a>') , books))
+    return '\n'.join(map(lambda book: emojize(':blue_book:' + book['title'] + book['author'] + '<a href="/get' + book['md5'] + '">[/get'+ book['md5'] +']</a>') , books))
 
-def test(bot, update, args):
-    results = api.search(' '.join(args))
+def search(bot, update, args):
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+    results = api.search(' '.join(args))
     bot.send_message(chat_id=update.message.chat_id, text=(emojize_book_list(results) if results  else 'No results Found!!'), parse_mode=ParseMode.HTML)
 
 
